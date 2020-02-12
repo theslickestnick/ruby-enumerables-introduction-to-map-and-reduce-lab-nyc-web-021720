4aadd8f73f5b7bfacddbcd664a4375341e912a78
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 20:52:19 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,74 @@
-# My Code here....
+require "pry"
+
+def map_to_negativize(source_array)
+  counter = 0
+  result = []
+  while counter < source_array.size do
+    # binding.pry
+    result << (source_array[counter] * -1)
+    counter += 1
+  end
+  result
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  counter = 0
+  result = []
+  while counter < source_array.size do
+    # binding.pry
+    result << (source_array[counter] * 2)
+    counter += 1
+  end
+  result
+end
+
+def map_to_square(source_array)
+  counter = 0
+  result = []
+  while counter < source_array.size do
+    # binding.pry
+    result << (source_array[counter] * source_array[counter])
+    counter += 1
+  end
+  result
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = 0
+  counter = 0
+
+  total = starting_point += source_array[counter]
+  counter += 1
+
+  while counter < source_array.size do
+    total = total += source_array[counter]
+    counter += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+  counter = 0
+  while counter < source_array.size do
+    if source_array[counter] == false
+      return false
+    end
+    counter += 1
+  end
+  source_array
+end
+
+def reduce_to_any_true(source_array)
+  counter = 0
+  while counter < source_array.size do
+    if source_array[counter] == true
+      return true
+    end
+    counter += 1
+  end
+  false
+end
