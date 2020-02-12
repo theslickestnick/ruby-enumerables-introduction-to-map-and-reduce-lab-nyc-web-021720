56097462970066f86d1bb5ed05db0dcd974cb653
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 20:26:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,7 @@ def reduce_to_total(source_array)
   total = 0
   while counter < source_array.size do
     # binding.pry
-    total += (total + source_array[counter])
+    total = total + source_array[counter]
     counter += 1
   end
   total
