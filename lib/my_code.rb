9c19e85ec5a81ebe2187cc565ff8e34ac1b949e4
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 20:36:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ def reduce_to_total(source_array, starting_point)
   counter = 0
   total = []
   while counter < source_array.size do
-    total = starting_point + source_array[counter]
+    total = total + source_array[counter]
     counter += 1
   end
   total
