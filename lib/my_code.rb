43a6b7cf3945eeff0de01855d689c38131846c41
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 20:19:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,3 +14,14 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  counter = 0
+  result = []
+  while counter < source_array.size do
+    # binding.pry
+    result << (source_array[counter] * 2)
+    counter += 1
+  end
+  result
+end
