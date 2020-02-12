3bacdebd4c5922218f4baa5c40f6f4c0578366e0
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 20:26:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,7 +37,7 @@ def map_to_square(source_array)
   result
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array)
   counter = 0
   total = 0
   while counter < source_array.size do
