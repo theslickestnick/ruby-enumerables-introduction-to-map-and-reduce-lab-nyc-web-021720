656a4d7f24546d2eff1ef7979b3c4bb20b65e0a1
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 20:50:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,5 +52,12 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
+  counter = 0
+  while counter < source_array.size do
+    if source_array[counter] == false
+      return false
+    end
+    counter += 1
+  end
   source_array
 end
