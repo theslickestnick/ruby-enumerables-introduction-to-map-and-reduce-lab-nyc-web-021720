9a0cc161ec59150b630dfcfd9e4a4d8d802f1618
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 20:15:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,7 @@ def map_to_negativize(source_array)
   result = []
   while counter < source_array.size do
     binding.pry
-    result << source_array[counter] * -1
+    result << (source_array[counter] * -1)
     counter +- 1
   end
   result
